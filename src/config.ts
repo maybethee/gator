1f09cb12a5855eded27b7e1fcc801d3b5fe7afb5
retra <jozalamun@gmail.com>
2026-01-11 09:28:01 -0500

    set a username to config file

---
+++ src/config.ts
@@ -0,0 +1,44 @@
+import fs from "fs";
+import os from "os";
+import path from "path";
+
+export type Config = {
+  dbUrl: string;
+  currentUserName: string;
+};
+
+export function setUser(name: string) {
+  //
+  const config = {
+    dbUrl: "postgres://example",
+    currentUserName: name,
+  };
+  writeConfig(config);
+
+  return config;
+}
+
+export function readConfig(): Config {
+  const filePath = getConfigFilePath();
+  const contents = fs.readFileSync(filePath, { encoding: "utf8" });
+  const validatedConfig = validateConfig(JSON.parse(contents));
+  return validatedConfig;
+}
+
+function getConfigFilePath(): string {
+  return path.join(os.homedir(), "/.gatorconfig.json");
+}
+
+function writeConfig(cfg: Config) {
+  const stringifiedConfig = JSON.stringify(cfg);
+  const filePath = getConfigFilePath();
+  fs.writeFileSync(filePath, stringifiedConfig);
+}
+
+function validateConfig(rawConfig): Config {
+  console.log(rawConfig);
+  return {
+    dbUrl: rawConfig.dbUrl,
+    currentUserName: rawConfig.currentUserName,
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,12 @@
+import { readConfig, setUser } from "./config.js";
+
 function main() {
-  console.log("Hello, world!");
+  setUser("name");
+  const cfg = readConfig();
+
+  for (const [key, val] of Object.entries(cfg)) {
+    console.log(`${key}: ${val}`);
+  }
 }
 
 main();
